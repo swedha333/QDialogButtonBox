065d1bc5a1bc7926e2389dadd4910766e9bf8ca3
KUMAR RAGHU VEER ROYAL AMARA <45122512+raghuveer-18@users.noreply.github.com>
2021-05-02 16:55:30 +0530

    Add files via upload

---
+++ QDialogButtonBox/Qdialougebox/QDialogbuttonbox/widget.cpp
@@ -0,0 +1,34 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include<infodialog.h>
+#include<QDebug>
+
+
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_showinfobutton_clicked()
+{
+    InfoDialog *dialog=new InfoDialog(this);
+    connect(dialog,&InfoDialog::accepted,[=](){
+
+    qDebug()<<"Dialog Accepted";
+
+    });
+    connect(dialog,&InfoDialog::rejected,[=](){
+
+    qDebug()<<"Dialog Rejected";
+
+    });
+    dialog->exec();
+}
